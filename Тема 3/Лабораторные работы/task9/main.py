9779c3950cc5884deeeae40d66045b2c3e6c1f35
PiniginArtem <Piniginartem206@gmail.com>
2023-03-30 18:00:26 +0300

    Theme3 Laboratory works 2

--- Тема 3/Лабораторные работы/task9/main.py
+++ Тема 3/Лабораторные работы/task9/main.py
@@ -1,8 +1,9 @@
 salary = 5000  # зарплата
 spend = 6000  # траты
 months = 10  # количество месяцев
-increase = 1.03  # рост цен
+increase = 0.03  # рост цен
 
+increase +=1 # добавим 100%, чтоб удобнее было умножать
 need_money = 0  # количество денег, чтобы прожить 10 месяцев
 
 for current_month in range(1, months+1):
--- Тема 3/Условная инструкция if/task11/main.py
+++ Тема 3/Условная инструкция if/task11/main.py
@@ -1,21 +1,15 @@
-month = 9
+month = 6
 
 spring_months = {3: 'Март',
                  4: 'Апрель',
                  5: 'Май'}  # TODO записать словарь весенних месяцев, где ключ это номер месяца, а значение его название
-summer_months = {6: 'Июнь',
-                 7: 'Июль',
-                 8: 'Август'}  # TODO записать список месяцев лета
-autumn_months = {9: 'Сентябрь',
-                 10: 'Октябрь',
-                 11: 'Ноябрь'}  # TODO записать кортеж месяцев осени
-winter_months = {12: 'Декабрь',
-                 1: 'Январь',
-                 2: 'Февраль'}  # TODO записать список месяцев зимы
+summer_months = "678"  # TODO записать список месяцев лета
+autumn_months = (9, 10, 11)  # TODO записать кортеж месяцев осени
+winter_months = [12, 1, 2]  # TODO записать список месяцев зимы
 
 if month in spring_months:
     print("Весна")
-elif month in summer_months:
+elif str(month) in summer_months:
     print("Лето")
 elif month in autumn_months:
     print("Осень")
